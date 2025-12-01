251b778edcf1ba48ed99626be14820a63bdbfde5
daniel <18262962+danieldonatsch@users.noreply.github.com>
2025-12-01 19:59:55 +0100

    Solutions Day 1

---
+++ solutions/day_x.py
@@ -0,0 +1,20 @@
+from . import base_solution as bs
+
+
+class SolutionDayXX(bs.BaseSolution):
+    def __init__(self, day_num: int, example=False, verbose=False):
+        super().__init__(day_num, example, verbose=verbose)
+
+    def _star_1(self) -> int:
+        """Solve puzzle 1
+
+        :return:
+        """
+        input_file_path = self.get_input_file_path()
+
+    def _star_2(self) -> int:
+        """Solve puzzle 2
+
+        :return:
+        """
+        input_file_path = self.get_input_file_path()
